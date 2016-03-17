a937c9ce35f6f72bb545bb5e50c0f3e2c390b096
Carlos Millett <carlos4735@gmail.com>
2016-03-17 11:37:48 -0300

    [bspwm] update config
    bspwm v0.9.1 breaks old configuration

--- bspwm/.config/bspwm/bin/updateConkyColors.sh
+++ bspwm/.config/bspwm/bin/updateConkyColors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env  bash
 
-OLD="$(grep -E -m 1 -o "#[A-Fa-f0-9]{6}" ${1})"
-NEW="#$(getXresColor color3)"
+OLD="$(grep -E -m 1 -o "#FF[A-Fa-f0-9]{6}" ${1})"
+NEW="#FF$(getXresColor color3)"
 
 sed -i "s;${OLD};${NEW};g" ${1}
