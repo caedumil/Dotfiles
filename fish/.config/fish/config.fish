f4c9ab70c709981ad5a8251d2fee54ffc1bed2fc
Carlos Millett <carlos4735@gmail.com>
2017-01-19 01:38:44 -0200

    [fish] fix config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -45,28 +45,3 @@ set __fish_git_prompt_char_untrackedfiles (set_color red)"●"(set_color normal)
 set __fish_git_prompt_char_stashstate (set_color blue)"●"(set_color normal)
 set __fish_git_prompt_char_upstream_ahead (set_color green)"↑"(set_color normal)
 set __fish_git_prompt_char_upstream_behind (set_color red)"↓"(set_color normal)
-
-#
-# Fish colors
-#
-#set fish_color_normal normal                        # the default color
-#set fish_color_command blue                         # commands
-#set fish_color_quote E6DB74                         # quoted blocks of text
-#set fish_color_redirection AE81FF                   # IO redirections
-#set fish_color_end F8F8F2                           # process separators like ';' and '&'
-#set fish_color_error F8F8F2 --background=F92672     # highlight potential errors
-#set fish_color_param A6E22E                         # regular command parameters
-#set fish_color_comment 75715E                       # code comments
-#set fish_color_match F8F8F2                         # highlight matching parenthesis
-#set fish_color_search_match --background=49483E     # highlight history search matches
-#set fish_color_operator AE81FF                      # parameter expansion operators like '*' and '~'
-#set fish_color_escape 66D9EF                        # highlight character escapes like '\n' and '\x70'
-#set fish_color_cwd 66D9EF                           # the current working directory in the default prompt
-#
-## Additionally, the following variables are available to change the
-## highlighting in the completion pager:
-#set fish_pager_color_prefix F8F8F2                  # prefix string, i.e. the string that is to be completed
-#set fish_pager_color_completion 75715E              # completion itself
-#set fish_pager_color_description 49483E             # completion description
-#set fish_pager_color_progress F8F8F2                # progress bar at the bottom left corner
-#set fish_pager_color_secondary F8F8F2               # background color of the every second completion
