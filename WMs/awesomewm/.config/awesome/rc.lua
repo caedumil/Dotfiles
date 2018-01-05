4c36089c3c08e5a6a6af32a81f5f672a3ebae585
Carlos Millett <carlos4735@gmail.com>
2018-01-05 21:06:06 -0200

    [WMs][awesomewm] update keybind

--- WMs/awesomewm/.config/awesome/rc.lua
+++ WMs/awesomewm/.config/awesome/rc.lua
@@ -167,20 +167,7 @@ awful.util.tasklist_buttons = gears.table.join(
             end
         end
     ),
-    awful.button({ }, 3,
-        function()
-            local instance = nil
-
-            return function()
-                if instance and instance.wibox.visible then
-                    instance:hide()
-                    instance = nil
-                else
-                    instance = awful.menu.clients({ theme = { width = 250 } })
-                end
-            end
-        end
-    ),
+    awful.button({ }, 3, function(c) c:kill() end),
     awful.button({ }, 4, function() awful.client.focus.byidx(1) end),
     awful.button({ }, 5, function() awful.client.focus.byidx(-1) end)
 )
@@ -435,7 +422,7 @@ globalkeys = gears.table.join(
         {description = "rofi menu launcher", group = "launcher"}
     ),
     awful.key(
-        { }, "XF86Mail",
+        { modkey, }, "XF86Mail",
         function() awful.spawn("sxlock -f '-misc-ohsnap-medium-*-normal-*-11-*-*-*-*-*-*-*'", false) end,
         {description = "lock screen", group = "launcher"}
     ),
