7b7c2f70dfe72990906d892d41f67e68c05750ba
Carlos Millett <carlos4735@gmail.com>
2018-01-07 13:31:01 -0200

    [WMs][awesomewm] update theme

--- WMs/awesomewm/.config/awesome/themes/osgiliath/theme.lua
+++ WMs/awesomewm/.config/awesome/themes/osgiliath/theme.lua
@@ -1,3 +1,4 @@
+-- vim:foldmethod=marker:foldlevel=0
 --[[
 
     AwesomeWM Theme
@@ -8,49 +9,59 @@
 --]]
 
 
-local gears = require("gears")
 local lain  = require("lain")
+local markup = lain.util.markup
 local awful = require("awful")
 local wibox = require("wibox")
-
 local xresources = require("beautiful.xresources")
 local xrdb = xresources.get_current_theme()
 
-theme                                          = {}
+
+theme                                           = {}
 theme.default_dir                               = require("awful.util").get_themes_dir() .. "default"
 theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/osgiliath"
-theme.wallpaper                                 = theme.dir .. "/wall"
+theme.wallpaper                                 = os.getenv("HOME") .. "/.wallpaper"
 theme.font                                      = "Artwiz Lime 8"
 theme.icon_font                                 = "Wuncon Siji 8"
-theme.fg_normal                                 = xrdb.color15
-theme.fg_focus                                  = xrdb.color7
-theme.fg_urgent                                 = xrdb.color0
-theme.bg_normal                                 = xrdb.color8
-theme.bg_focus                                  = xrdb.color0
-theme.bg_urgent                                 = xrdb.color1
-theme.border_width                              = 5
-theme.border_normal                             = theme.bg_normal
-theme.border_focus                              = theme.bg_focus
-theme.taglist_fg_empty                          = theme.fg_normal
-theme.taglist_fg_focus                          = theme.bg_focus
-theme.taglist_fg_occupied                       = theme.bg_focus
-theme.taglist_bg_empty                          = theme.bg_normal
-theme.taglist_bg_focus                          = theme.fg_focus
-theme.taglist_bg_occupied                       = theme.bg_normal
-theme.menu_height                               = 16
-theme.menu_width                                = 140
-theme.ocol                                      = "<span color='" .. theme.fg_normal .. "'>"
-theme.tasklist_sticky                           = theme.ocol .. "[S]</span>"
-theme.tasklist_ontop                            = theme.ocol .. "[T]</span>"
-theme.tasklist_floating                         = theme.ocol .. "[F]</span>"
-theme.tasklist_maximized_horizontal             = theme.ocol .. "[M] </span>"
-theme.tasklist_maximized_vertical               = ""
-theme.tasklist_disable_icon                     = true
 theme.awesome_icon                              = theme.dir .."/icons/awesome.png"
 theme.menu_submenu_icon                         = theme.dir .."/icons/submenu.png"
+theme.useless_gap                               = 10
+theme.fg_normal                                 = xrdb.color7
+theme.bg_normal                                 = xrdb.color0
+theme.fg_normal_alt                             = xrdb.color8
+theme.fg_focus                                  = xrdb.color0
+theme.bg_focus                                  = xrdb.color1
+theme.fg_urgent                                 = xrdb.color3
+theme.bg_urgent                                 = xrdb.color0
+theme.border_normal                             = theme.bg_normal
+theme.border_focus                              = theme.fg_normal_alt
+theme.border_width                              = 5
+theme.taglist_fg_empty                          = theme.fg_normal_alt
+theme.taglist_bg_empty                          = theme.bg_normal
+theme.taglist_fg_focus                          = theme.fg_focus
+theme.taglist_bg_focus                          = theme.bg_focus
+theme.taglist_fg_occupied                       = theme.bg_focus
+theme.taglist_bg_occupied                       = theme.bg_normal
 theme.taglist_squares_sel                       = ""
 theme.taglist_squares_unsel                     = ""
-theme.useless_gap                               = 10
+theme.menu_fg_normal                            = theme.fg_normal
+theme.menu_bg_normal                            = theme.bg_normal
+theme.menu_fg_focus                             = theme.fg_focus
+theme.menu_bg_focus                             = theme.bg_focus
+theme.menu_height                               = 16
+theme.menu_width                                = 140
+theme.tasklist_fg_normal                        = theme.fg_normal
+theme.tasklist_bg_normal                        = theme.bg_normal
+theme.tasklist_fg_focus                         = theme.bg_focus
+theme.tasklist_bg_focus                         = theme.fg_focus
+theme.tasklist_fg_minimize                      = theme.fg_normal_alt
+theme.tasklist_bg_minimize                      = theme.bg_normal
+theme.tasklist_disable_icon                     = true
+theme.tasklist_sticky                           = markup(theme.fg_normal, "[S]")
+theme.tasklist_ontop                            = markup(theme.fg_normal, "[T]")
+theme.tasklist_floating                         = markup(theme.fg_normal, "[F]")
+theme.tasklist_maximized_horizontal             = markup(theme.fg_normal, "[M]")
+theme.tasklist_maximized_vertical               = markup(theme.fg_normal, "[V]")
 theme.layout_txt_floating                       = "\u{E0B1}"
 theme.layout_txt_tile                           = "\u{E002}"
 theme.layout_txt_tileleft                       = "[tl]"
@@ -75,6 +86,10 @@ theme.widget_net                                = "\u{E19C}"
 theme.widget_netdown                            = "\u{E07B}"
 theme.widget_netup                              = "\u{E07A}"
 theme.widget_vol                                = "\u{E203}"
+theme.titlevar_fg_normal                        = theme.fg_normal
+theme.titlebar_bg_normal                        = theme.bg_normal
+theme.titlebar_fg_focus                         = theme.fg_focus
+theme.titlebar_bg_focus                         = theme.fg_normal_alt
 theme.titlebar_close_button_normal              = theme.default_dir.."/titlebar/close_normal.png"
 theme.titlebar_close_button_focus               = theme.default_dir.."/titlebar/close_focus.png"
 theme.titlebar_minimize_button_normal           = theme.default_dir.."/titlebar/minimize_normal.png"
@@ -95,25 +110,26 @@ theme.titlebar_maximized_button_normal_inactive = theme.default_dir.."/titlebar/
 theme.titlebar_maximized_button_focus_inactive  = theme.default_dir.."/titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_active   = theme.default_dir.."/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active    = theme.default_dir.."/titlebar/maximized_focus_active.png"
+theme.notification_fg                           = theme.fg_normal
 theme.notification_bg                           = theme.bg_normal
-theme.notification_fg                           = theme.fg_focus
-theme.notification_border_color                 = theme.bg_focus
+theme.notification_border_color                 = theme.fg_normal_alt
 theme.notification_border_width                 = theme.border_width
 
-local markup = lain.util.markup
-local white  = theme.fg_focus
-local gray   = theme.fg_normal
+-- Panel widgets {{{
+local color_bright  = theme.fg_normal
+local color_normal  = theme.fg_normal_alt
+local color_sep     = theme.bg_focus
 
 -- {{{ Textclock
-local textclockdate = wibox.widget.textclock(markup(white, " %A, %d %B %Y "))
-local textclocktime = wibox.widget.textclock(markup(white, " %H:%M "))
+local textclockdate = wibox.widget.textclock(markup(color_bright, " %A, %d %B %Y "))
+local textclocktime = wibox.widget.textclock(markup(color_bright, " %H:%M "))
 local textclockdateicon = wibox.widget({
-    text = theme.widget_date,
+    markup = markup(color_normal, theme.widget_date),
     font = theme.icon_font,
     widget = wibox.widget.textbox
 })
 local textclocktimeicon = wibox.widget({
-    text = theme.widget_time,
+    markup = markup(color_normal, theme.widget_time),
     font = theme.icon_font,
     widget = wibox.widget.textbox
 })
@@ -126,7 +142,7 @@ lain.widget.calendar({
     attach_to = { textclockdate },
     notification_preset = {
         font = theme.font,
-        fg   = white,
+        fg   = theme.fg_normal,
         bg   = theme.bg_normal
     }
 })
@@ -134,13 +150,13 @@ lain.widget.calendar({
 
 -- {{{ MPD
 local mpdicon = wibox.widget({
-    text = theme.widget_mpd,
+    markup = markup(color_normal, theme.widget_mpd),
     font = theme.icon_font,
     widget = wibox.widget.textbox
 })
-theme.mpd = lain.widget.mpd({
+local mpd = lain.widget.mpd({
     settings = function()
-        mpd_notification_preset.fg = white
+        mpd_notification_preset.fg = color_bright
 
         artist = mpd_now.artist .. " "
         title  = mpd_now.title  .. " "
@@ -153,20 +169,20 @@ theme.mpd = lain.widget.mpd({
             title  = ""
         end
 
-        widget:set_markup(markup.font(theme.font, markup(gray, artist) .. markup(white, title)))
+        widget:set_markup(markup.font(theme.font, markup(color_normal, artist) .. markup(color_bright, title)))
     end
 })
 -- }}}
 
 -- {{{ CPU
 local cpuicon = wibox.widget({
-    text = theme.widget_cpu,
+    markup = markup(color_normal, theme.widget_cpu),
     font = theme.icon_font,
     widget = wibox.widget.textbox
 })
 local cpu = lain.widget.cpu({
     settings = function()
-        local info = markup.fontfg(theme.font, white, cpu_now.usage .. "% ")
+        local info = markup.fontfg(theme.font, color_bright, cpu_now.usage .. "% ")
         widget:set_markup(info)
     end
 })
@@ -174,25 +190,25 @@ local cpu = lain.widget.cpu({
 
 -- {{{ Net
 local neticon = wibox.widget({
-    text = theme.widget_net,
+    markup = markup(color_normal, theme.widget_net),
     font = theme.icon_font,
     widget = wibox.widget.textbox
 })
 local netdownicon = wibox.widget({
-    text = theme.widget_netdown,
+    markup = markup(color_normal, theme.widget_netdown),
     font = theme.icon_font,
     widget = wibox.widget.textbox
 })
 local netupicon = wibox.widget({
-    text = theme.widget_netup,
+    markup = markup(color_normal, theme.widget_netup),
     font = theme.icon_font,
     widget = wibox.widget.textbox
 })
 local netdowninfo = wibox.widget.textbox()
 local netupinfo = lain.widget.net({
     settings = function()
-        local up = markup.fontfg(theme.font, white, net_now.sent .. " ")
-        local dn = markup.fontfg(theme.font, white, net_now.received .. " ")
+        local up = markup.fontfg(theme.font, color_bright, net_now.sent .. " ")
+        local dn = markup.fontfg(theme.font, color_bright, net_now.received .. " ")
 
         widget:set_markup(up)
         netdowninfo:set_markup(dn)
@@ -202,13 +218,13 @@ local netupinfo = lain.widget.net({
 
 -- {{{ PULSE
 local pulseicon = wibox.widget({
-    text = theme.widget_vol,
+    markup = markup(color_normal, theme.widget_vol),
     font = theme.icon_font,
     widget = wibox.widget.textbox
 })
 local pulsevol = lain.widget.pulse({
     settings = function()
-        local fg = volume_now.muted == "no" and white or gray
+        local fg = volume_now.muted == "no" and color_bright or color_normal
         local info = string.format("%s:%s", volume_now.left, volume_now.right)
         local markinfo = markup.fontfg(theme.font, fg, info .. " ")
         widget:set_markup(markinfo)
@@ -218,7 +234,7 @@ local pulsevol = lain.widget.pulse({
 
 -- {{{ Separator
 local spricon = wibox.widget({
-    text = theme.widget_sep,
+    markup = markup(color_sep, theme.widget_sep),
     font = theme.icon_font,
     widget = wibox.widget.textbox
 })
@@ -226,108 +242,33 @@ local spricon = wibox.widget({
 local spr = wibox.container.margin(spricon, 5, 5)
 -- }}}
 
--- {{{ Wibar
-local function update_txt_layoutbox(s)
-    -- Writes a string representation of the current layout in a textbox widget
-    local txt_l = theme["layout_txt_" .. awful.layout.getname(awful.layout.get(s))] or ""
-    s.mytxtlayoutbox:set_markup(markup(white, txt_l))
-end
-
-function theme.at_screen_connect(s)
-    -- Wallpaper
-    set_wallpaper(s)
-
-    -- Tags
-    awful.tag(tags.names, s, tags.layout)
-
-    -- Textual layoutbox
-    s.mytxtlayoutbox = wibox.widget({
-        font = theme.icon_font,
-        widget = wibox.widget.textbox
-    })
-    update_txt_layoutbox(s)
-    awful.tag.attached_connect_signal(s,
-                                      "property::selected",
-                                      function() update_txt_layoutbox(s) end
-                                     )
-    awful.tag.attached_connect_signal(s,
-                                      "property::layout",
-                                      function() update_txt_layoutbox(s) end
-                                     )
-    s.mytxtlayoutbox:buttons(
-        gears.table.join(
-            awful.button({}, 1, function() awful.layout.inc(1) end),
-            awful.button({}, 3, function() awful.layout.inc(-1) end),
-            awful.button({}, 4, function() awful.layout.inc(1) end),
-            awful.button({}, 5, function() awful.layout.inc(-1) end)
-        )
-    )
-
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist(s,
-                                       awful.widget.taglist.filter.all,
-                                       awful.util.taglist_buttons,
-                                       { font = theme.icon_font }
-                                      )
-
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist(s,
-                                         awful.widget.tasklist.filter.currenttags,
-                                         awful.util.tasklist_buttons,
-                                         { align = "center" }
-                                        )
-
-    -- Create the wibox
-    s.mywibox = awful.wibar(
-        {
-            position = "top",
-            screen = s,
-            height = 20,
-            bg = theme.bg_normal,
-            fg = theme.fg_normal
-        }
-    )
-
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-        layout = wibox.layout.align.horizontal,
-        {   -- Left widgets
-            layout = wibox.layout.fixed.horizontal,
-            spr,
-            s.mytaglist,
-            spr,
-            s.mytxtlayoutbox,
-            spr,
-        },
-        s.mytasklist, -- Middle widget
-        {   -- Right widgets
-            layout = wibox.layout.fixed.horizontal,
-            spr,
-            mpdicon,
-            theme.mpd.widget,
-            spr,
-            cpuicon,
-            cpu,
-            spr,
-            pulseicon,
-            pulsevol,
-            spr,
-            neticon,
-            netdownicon,
-            netdowninfo,
-            netupicon,
-            netupinfo,
-            spr,
-            textclockdateicon,
-            textclockdate,
-            textclocktimeicon,
-            textclocktime,
-            spr,
-        },
-    }
-end
+-- Export panel{{{
+theme.wibox = {}
+theme.wibox.sep = spr
+theme.wibox.rightside = {
+    spr,
+    mpdicon,
+    mpd.widget,
+    spr,
+    cpuicon,
+    cpu,
+    spr,
+    pulseicon,
+    pulsevol,
+    spr,
+    neticon,
+    netdownicon,
+    netdowninfo,
+    netupicon,
+    netupinfo,
+    spr,
+    textclockdateicon,
+    textclockdate,
+    textclocktimeicon,
+    textclocktime,
+    spr,
+}
+-- }}}
 -- }}}
 
 return theme
-
--- vim:foldmethod=marker:foldlevel=0
