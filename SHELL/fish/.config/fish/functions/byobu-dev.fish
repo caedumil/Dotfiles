5c610d7715c84fc503431e7e0e181069b3116253
Carlos Millett <carlos4735@gmail.com>
2020-01-28 03:38:49 -0300

    [SHELL][fish] delete aliases
    - byobu-tmux
    - tmux-dev

--- SHELL/fish/.config/fish/functions/byobu-dev.fish
+++
@@ -1,7 +0,0 @@
-function byobu-dev --description "Start byobu session at specified directory"
-    set -l dir (realpath $argv)
-    if test ! -d $dir
-        return 1
-    end
-    byobu-tmux new-session -A -c $dir -s (basename $dir)
-end
--- SHELL/fish/.config/fish/functions/tmux-dev.fish
+++
@@ -1,7 +0,0 @@
-function tmux-dev --description "Start tmux session at specified directory"
-    set -l dir (realpath $argv)
-    if test ! -d $dir
-        return 1
-    end
-    tmux new-session -A -c $dir -s (basename $dir)
-end
