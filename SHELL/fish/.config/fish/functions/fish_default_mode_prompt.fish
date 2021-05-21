dcf37546a7b5934fd8870bc6b53ecd4356f01813
Carlos Millett <carlos4735@gmail.com>
2021-05-20 21:19:51 -0300

    [SHELL][fish] tweak fish_default_mode_prompt.fish

--- SHELL/fish/.config/fish/functions/fish_default_mode_prompt.fish
+++ SHELL/fish/.config/fish/functions/fish_default_mode_prompt.fish
@@ -1,21 +1,19 @@
 function fish_default_mode_prompt --description "Display the default mode for the prompt"
-    # Do nothing if not in vi mode
-    if test "$fish_key_bindings" = "fish_vi_key_bindings"
-        or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+    if test "$fish_key_bindings" = "fish_vi_key_bindings" -o "$fish_key_bindings" = "fish_hybrid_key_bindings"
         switch $fish_bind_mode
             case insert
                 true
             case default
-                set_color --bold --background red white
+                set_color --background red white
                 echo -n '[N]'
             case replace_one
-                set_color --bold --background green white
+                set_color --background green white
                 echo -n '[R]'
             case replace
-                set_color --bold --background cyan white
+                set_color --background cyan white
                 echo -n '[R]'
             case visual
-                set_color --bold --background magenta white
+                set_color --background magenta white
                 echo -n '[V]'
         end
     else
